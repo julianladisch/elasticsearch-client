8387b6c7731dd1fe16f795c353079367daeab1d6
Julian Ladisch <julianladisch@users.noreply.github.com>
2024-09-02 18:51:00 +0200

    Add code

---
+++ src/test/java/test/ElasticsearchClientTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.junit.jupiter.api.Test;
+import org.opensearch.testcontainers.OpensearchContainer;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+class ElasticsearchClientTest {
+
+  static final String ELASTICSEARCH_USERNAME = "elastic";
+  static final String ELASTICSEARCH_PASSWORD = "changeme";
+
+  @Container
+  ElasticsearchContainer elasticsearch = new ElasticsearchContainer(
+      DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.15.0"));
+
+  @Container
+  OpensearchContainer<?> opensearch = new OpensearchContainer<>(
+      DockerImageName.parse("opensearchproject/opensearch:2.16.0"));
+
+  @Test
+  void test() throws IOException {
+    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(
+        AuthScope.ANY,
+        new UsernamePasswordCredentials(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD)
+    );
+
+    RestClient client =
+        RestClient
+            .builder(HttpHost.create("https://" + elasticsearch.getHttpHostAddress()))
+            .setHttpClientConfigCallback(httpClientBuilder -> {
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                httpClientBuilder.setSSLContext(elasticsearch.createSslContextFromCa());
+                return httpClientBuilder;
+            })
+            .build();
+
+    Response response = client.performRequest(new Request("GET", "/_cluster/health"));
+    assertEquals(200, response.getStatusLine().getStatusCode(), () -> response.toString());
+  }
+}
